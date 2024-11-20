ef8477ed8f2461e438018c652f0277136d313531
Em.X <edges@gmx.com>
2024-11-20 17:40:48 +0100

    fonction d'import des ships, TODO: test

--- scripts/imports_ships.php
+++ scripts/imports_ships.php
@@ -1,45 +1,51 @@
 <?php
 
 use Php\Ship;
+
 include("../config.php");
 
 function import_ships(string $json)
 {
+    global $dbh;
     $jsonData = json_decode(file_get_contents($json), true);
-    global $dbh;
-    foreach ($jsonData as $shipData) {
-        //truncate table
-        $truncate = "TRUNCATE TABLE ships;";
-        $stmt = $dbh->prepare($truncate);
-        $stmt->execute();
-        echo "Table 'ship' has been emptied.";
-        //create ship -> valid field data
-        $ship = new Ship(
-            $shipData["id"],
-            $shipData["name"],
-            $shipData["camp"],
-            $shipData["speed_kmh"],
-            $shipData["capacity"]
-        );
-        //import valid ship to database
-        import_ship_to_database($ship);
-    }
-}
 
-function import_ship_to_database(Ship $ship) {
-    global $dbh;
     try {
-        $insert = $dbh->prepare("INSERT INTO ship(id, name, camp, speed_kmh, capacity) VALUES(:id, :name, :camp, :speed_kmh, :capacity);");
-        $stmt = $dbh->prepare($insert);
-        $stmt->bindParam(":id", $ship["id"], PDO::PARAM_INT);
-        $stmt->bindParam(":name", $ship["name"], PDO::PARAM_STR);
-        $stmt->bindParam(":camp", $ship["camp"], PDO::PARAM_STR);
-        $stmt->bindParam(":speed_kmh", $ship["speed_kmh"], PDO::PARAM_INT);
-        $stmt->bindParam(":capacity", $ship["capacity"], PDO::PARAM_INT);
-        $stmt->execute();
-        echo "Ship " . $ship["id"] . " has been imported.";
-    } catch (PDOException $e) {
-        echo "Failed to insert ship in database: " . $e->getMessage();
-    }
+        // Begin transaction
+        $dbh->beginTransaction();
 
+        // Empty table before filling
+        $dbh->execute("TRUNCATE TABLE ship;");
+
+        // Prepare statement for insert
+        $stmt = $dbh->prepare(
+            "INSERT INTO ship (id, name, camp, speed_kmh, capacity)
+             VALUES (:id, :name, :camp, :speed_kmh, :capacity);"
+        );
+
+        // Iterate json file to create ship
+        foreach ($jsonData as $shipData) {
+            // If ship is sucessfully created, proceed
+            $ship = new Ship(
+                $shipData["id"],
+                $shipData["name"],
+                $shipData["camp"],
+                $shipData["speed_kmh"],
+                $shipData["capacity"]
+            );
+            // Bind parameters and execute
+            $stmt->bindParam(":id", $shipData["id"], PDO::PARAM_INT);
+            $stmt->bindParam(":name", $shipData["name"], PDO::PARAM_STR);
+            $stmt->bindParam(":camp", $shipData["camp"], PDO::PARAM_STR);
+            $stmt->bindParam(":speed_kmh", $shipData["speed_kmh"], PDO::PARAM_INT);
+            $stmt->bindParam(":capacity", $shipData["capacity"], PDO::PARAM_INT);
+            $stmt->execute();
+        }
+
+        // Commit transaction
+        $dbh->commit();
+
+    } catch (PDOException $e) {
+        $dbh->rollBack();
+        echo $e->getMessage();
+    }
 }
