d3ccdad33af17ce18a1ed28d873dcb391c2f0733
Em.X <edges@gmx.com>
2025-02-14 10:18:55 +0100

    password validity

--- utils/utils.js
+++ utils/utils.js
@@ -1,15 +1,39 @@
-const passInput = document.getElementById("pswd");
+/*
+UTILS.JS CONTAINS
+    - Password eye toggle to show/hide value
+    - Password validity
+ */
+
+// SHOW PASSWORD
+const passInput = document.getElementById("pass");
 const showPass = document.querySelector(".fa-eye");
 const hidePass = document.querySelector(".fa-eye-slash");
 
 showPass.addEventListener("click", () => {
-    passInput.type = "text";
-    hidePass.classList.remove("hidden");
-    showPass.classList.add("hidden");
+    showPass.classList.toggle("fa-eye");
+    showPass.classList.toggle("fa-eye-slash");
+    passInput.type = passInput.type == "password" ? "text" : "password";
 })
 
-hidePass.addEventListener("click", () => {
-    passInput.type = "password";
-    hidePass.classList.add("hidden");
-    showPass.classList.remove("hidden");
+// VERIFY PASSWORD STRENGTH, src : https://www.youtube.com/watch?v=Iss2ASrpl9s
+const validationRegex = [
+        { regex : /.{12,}/ }, // min length
+        { regex : /[0-9]/ }, // numbers
+        { regex : /[a-z]/ }, // lowercase letters
+        { regex : /[A-Z]/ }, // upppercse letters
+        { regex : /[^a-zA-Z0-9]/ } // special characters
+]
+
+let passChecklist = document.querySelectorAll(".pass-info");
+
+passInput.addEventListener("keyup", () => {
+    validationRegex.forEach((item, i) => {
+        let isValid = item.regex.test(passInput.value);
+
+        if( isValid ) {
+            passChecklist[i].classList.add("checked");
+        } else {
+            passChecklist[i].classList.remove("checked");
+        }
+    })
 })
