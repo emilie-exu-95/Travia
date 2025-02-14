e26d58db59f8bc2ee3449c3fab5cc0e00d42cdbd
Em.X <edges@gmx.com>
2025-02-14 11:55:08 +0100

    password strength using an API

--- utils/utils.js
+++ utils/utils.js
@@ -15,7 +15,7 @@ showPass.addEventListener("click", () => {
     passInput.type = passInput.type == "password" ? "text" : "password";
 })
 
-// VERIFY PASSWORD STRENGTH, src : https://www.youtube.com/watch?v=Iss2ASrpl9s
+// VERIFY PASSWORD CONDITIONS, src : https://www.youtube.com/watch?v=Iss2ASrpl9s
 const validationRegex = [
         { regex : /.{12,}/ }, // min length
         { regex : /[0-9]/ }, // numbers
@@ -24,7 +24,7 @@ const validationRegex = [
         { regex : /[^a-zA-Z0-9]/ } // special characters
 ]
 
-let passChecklist = document.querySelectorAll(".pass-info");
+const passChecklist = document.querySelectorAll(".pass-info");
 
 passInput.addEventListener("keyup", () => {
     validationRegex.forEach((item, i) => {
@@ -36,4 +36,31 @@ passInput.addEventListener("keyup", () => {
             passChecklist[i].classList.remove("checked");
         }
     })
+})
+
+// VERIFY PASSWORD STRENGTH, not the best
+const passSuggestion = document.querySelector(".pass-suggestion");
+const passStrength = document.getElementById("pass-strength");
+const suggestion = document.getElementById("suggestion");
+const strengthLevels = ["Very Weak", "Weak", "Fair", "Strong", "Very Strong"];
+const strengthColors = ["#FF4C4C", "#FF4C4C", "#FF9F40", "#3DDC97", "#00C853"];
+passInput.addEventListener("input", () => {
+    let password = passInput.value;
+    if ( !password ) { // Empty input
+        passSuggestion.classList.add("hidden");
+        passStrength.textContent = "";
+        suggestion.textContent = "";
+        return;
+    }
+    // Show suggestion at input
+    passSuggestion.classList.remove("hidden");
+    let result = zxcvbn(password);
+    passStrength.textContent = strengthLevels[result.score];
+    passStrength.style.color = strengthColors[result.score]
+    let string = " : ";
+    if (result.score > 2) {
+        string = "";
+    }
+    suggestion.textContent = string + result.feedback.suggestions;
+
 })
